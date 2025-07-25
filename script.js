805b323de5f69dfd74aa9d032a43595b59d5d033
shim079 <150309430+shim079@users.noreply.github.com>
2025-07-25 09:49:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+  let menu = document.querySelector('#menu-icon');
+let navbar =document.querySelector('navbar');
 
+menu.onclick =() =>{
+menu.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+
+}
+window.onscroll =() =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+      strings: [' FrontEnd Developer', 'BackEnd Developer' ,'Content Creator' ,'Graphic Designer'],
+      typeSpeed: 80,
+      backSpeed: 80,
+      backdelay: 1200,
+      loop: true,
+    });
